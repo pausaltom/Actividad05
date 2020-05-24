f3eb00806bddae99a8c0f3c7d710f82f2866bde1
Pau Saltó <pausaltom@gmail.com>
2020-05-24 18:04:44 +0200

    Programa terminado sin ordenar ni preguntar al usuario

--- src/base/Circulo.java
+++ src/base/Circulo.java
@@ -6,6 +6,15 @@ public class Circulo {
     public Circulo(double radio) {
         this.radio = radio;
     }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+    
     
    public double area(){
        return Math.PI*Math.pow(radio, 2);
@@ -13,4 +22,11 @@ public class Circulo {
    public double perimetro(){
        return Math.PI*radio*2;
    }
+
+    @Override
+    public String toString() {
+        return String.format("%s%.4f%n%s%.4f%n", "Area: ",area(),"Perimetro: ",perimetro());
+    }
+   
+   
 }
--- src/base/UF5_A01_Pau_SaltóMestre.java
+++ src/base/UF5_A01_Pau_SaltóMestre.java
@@ -1,16 +1,44 @@
 package base;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
 
 public class UF5_A01_Pau_SaltóMestre {
 
+    private static ArrayList<Double> listaRadios = new ArrayList<Double>();
+    private static ArrayList<Circulo> listaCirculos = new ArrayList<Circulo>();
+    private static Scanner leerScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-            double radio = aleatorio();
-            System.out.println(radio);
-            Circulo circulo1 = new Circulo(radio);
-            System.out.println(circulo1.area()+ "\n" +circulo1.perimetro());
+        int sino = 0;
+        double radio;
         
+            do {
+                radio = aleatorio();
+
+                listaRadios.add(radio);
+                Circulo circulo1 = new Circulo(radio);
+                listaCirculos.add(circulo1);
+
+            } while (radio >= 0.01);
+
+
+           
+            
+            for (Circulo lista : listaCirculos) {
+               
+                System.out.println(lista.toString());
+                
+            }
+
+            
+
+        
+
     }
-    public static double aleatorio(){
+
+    public static double aleatorio() {
         return Math.random();
     }
 
